c2b55db1a2a81a68f7e476fff679e6c56c946da6
graybeasley <grayton.beasley@g.austincc.edu>
2025-11-02 00:47:50 +0000

    HW:06 TicTacToe

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,28 @@
+#include "tic_tac_toe.h"
+using std::string;
+using std::cin;
+using std::cout;
 
 int main() 
 {
+    TicTacToe tic_tac_toe;
 
-	return 0;
+    string first_player;
+    cout << "Enter first player (X or O): ";
+    cin >> first_player;
+
+    tic_tac_toe.start_game(first_player);
+
+    while (!tic_tac_toe.game_over())
+    {
+        int position;
+        cout << "Enter position (1-9): ";
+        cin >> position;
+
+        tic_tac_toe.mark_board(position);
+        tic_tac_toe.display_board();
+    }
+
+    cout << "Game over!\n";
+    return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,75 @@
-//cpp
+#include "tic_tac_toe.h"
+
+bool TicTacToe::game_over()
+{
+    return check_board_full();
+}
+
+void TicTacToe::start_game(string first_player)
+{
+    if (first_player == "X" || first_player == "O")
+    {
+        player = first_player;
+        clear_board();
+    }
+}
+
+void TicTacToe::mark_board(int position)
+{
+    pegs[position - 1] = player;
+    set_next_player();
+}
+
+string TicTacToe::get_player() const
+{
+    return player;
+}
+
+void TicTacToe::display_board() const
+{
+    for (int i = 0; i < 9; i++)
+    {
+        cout << pegs[i];
+        if ((i + 1) % 3 == 0)
+        {
+            cout << "\n";
+        }
+        else
+        {
+            cout << " | ";
+        }
+    }
+    cout << "\n";
+}
+
+void TicTacToe::set_next_player()
+{
+    if (player == "X")
+    {
+        player = "O";
+    }
+    else
+    {
+        player = "X";
+    }
+}
+
+bool TicTacToe::check_board_full()
+{
+    for (auto peg : pegs)
+    {
+        if (peg == " ")
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void TicTacToe::clear_board()
+{
+    for (auto &peg : pegs)
+    {
+        peg = " ";
+    }
+}
