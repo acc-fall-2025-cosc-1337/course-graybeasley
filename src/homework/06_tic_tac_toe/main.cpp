b4a891c7a8d041a94a5c6383eef0bad0faf0493e
graybeasley <grayton.beasley@g.austincc.edu>
2025-11-09 18:03:27 +0000

    HW 07: TicTacToe Win Detection Correct Branch

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,40 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::string;
 
-int main() 
+int main()
 {
+    string first_player;
+    char choice = 'y';
 
-	return 0;
+    while (choice == 'y' || choice == 'Y')
+    {
+        TicTacToe game;
+        do
+        {
+            cout << "Enter first player (X or O): ";
+            cin >> first_player;
+        } while (first_player != "X" && first_player != "O");
+
+        game.start_game(first_player);
+
+        int position;
+        while (!game.game_over())
+        {
+            cout << "Enter position (1-9): ";
+            cin >> position;
+            game.mark_board(position);
+            game.display_board();
+        }
+
+        cout << "Game over! Winner: " << game.get_winner() << "\n";
+
+        cout << "Play again? (y/n): ";
+        cin >> choice;
+    }
+
+    cout << "Goodbye!\n";
+    return 0;
 }
